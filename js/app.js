9ea2f48146fb96b0f9090c1da46744a76a5701f1
Perry Brandiezs <pbrandiezs@gmail.com>
2019-07-17 14:31:25 -0700

    feat: added doc reference to timer function

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 function timer(){
+    // Modified from reference at stackoverflow https://stackoverflow.com/questions/31559469/how-to-create-a-simple-javascript-timer
     var sec = 0;
     var timer = setInterval(function(){
         document.getElementById('time').innerHTML='Seconds: ' + sec;
