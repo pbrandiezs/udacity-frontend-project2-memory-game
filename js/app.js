fdc60e951b0831a829e4f5dd80233f787484c261
Perry Brandiezs <pbrandiezs@gmail.com>
2019-07-18 11:19:31 -0700

    feat: Added shuffle function

--- js/app.js
+++ js/app.js
@@ -39,5 +39,26 @@ function stopTimer() {
     clearInterval(timer);
 }
 
+function shuffle(a) {
+    // Fisher-Yates shuffle algorithm from stackoverflow https://stackoverflow.com/questions/6274339/how-can-i-shuffle-an-array
+    /**
+     * Shuffles array in place.
+     * @param {Array} a items An array containing the items.
+     */
+    var j, x, i;
+    for (i = a.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = a[i];
+        a[i] = a[j];
+        a[j] = x;
+    }
+    return a;
+}
+
+
+
+var cardValues=["A", "B", "C", "D", "E", "F", "G", "H", "A", "B", "C", "D", "E", "F", "G", "H"];
 
 timer();
+shuffle(cardValues);
+console.log(cardValues);
