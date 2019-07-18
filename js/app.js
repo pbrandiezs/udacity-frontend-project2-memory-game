2d5e6de5fe2c06d0efae7768abd150511c9e3839
Perry Brandiezs <pbrandiezs@gmail.com>
2019-07-18 14:56:49 -0700

    feat: Check for faceup card

--- js/app.js
+++ js/app.js
@@ -58,11 +58,43 @@ function shuffle(a) {
 
 
 var cardValues=["A", "B", "C", "D", "E", "F", "G", "H", "A", "B", "C", "D", "E", "F", "G", "H"];
-var card0 = document.querySelector('.card0');
-card0.addEventListener('click', function () {
-    console.log('card0 clicked!');
-    console.log(cardValues[0]);
-});
+var faceUp=[false, false, false, false, false, false, false, false, false, false, false, false, false, false, false];
+
+const cards = document.querySelectorAll('.cards');
+var guess = [];
+var guessCount = 0;
+
+var clickCardFunction = function (event) {
+    console.log('Clicked!');
+    card = event.target.textContent;
+    console.log(card);
+    console.log(cardValues[card - 1]);
+    if (faceUp[card - 1] === false) {
+        faceUp[card - 1] = true;
+    } else {
+        console.log("Card already faceup, not allowed!");
+        return;
+    }
+    if (guessCount === 0) {
+        guess[0] = cardValues[card - 1];
+        guessCount++;
+    } else {
+        guess[1] = cardValues[card - 1];
+        guessCount = 0;
+        if (guess[0] === guess[1]) {
+            console.log("Match!");
+        } else {
+            console.log("No Match!");
+        }
+    }
+
+};
+
+for (i=0;i<=15;i++) {
+    cards[i].addEventListener('click', clickCardFunction, false);
+}
+
+
 
 timer();
 shuffle(cardValues);
